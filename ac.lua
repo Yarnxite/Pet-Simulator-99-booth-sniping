36251ba3b6ebb393a27eaa1fb674ee0597b7fd89
Yarnxite <91809491+Yarnxite@users.noreply.github.com>
2023-12-28 20:33:44 -0500

    Update ac.lua

--- ac.lua
+++ ac.lua
@@ -55,10 +55,10 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
     end
 
     if boughtPet == true then
-	local webcolor = 33dd99
+	local webcolor = tonumber(0x33dd99)
 	local weburl = webhook
     else
-	local webcolor = ff0000
+	local webcolor = tonumber(0xff0000)
 	local weburl = webhookFail
     end
     
